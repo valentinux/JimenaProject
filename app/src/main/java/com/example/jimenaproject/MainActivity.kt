fb772b7efecee551b560a02a777ede48d943fbc9
Valentinux <valentinux81@gmail.com>
2022-06-11 07:37:41 +0200

    Versión con splash screen

--- app/src/main/java/com/example/jimenaproject/MainActivity.kt
+++ app/src/main/java/com/example/jimenaproject/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        setTheme(R.style.Theme_JimenaProject)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
