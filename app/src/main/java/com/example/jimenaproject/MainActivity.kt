4150ecb9dcd9dd9242c3a9e73c1f1638fd83917d
Valentinux <valentinux81@gmail.com>
2022-06-19 06:18:52 +0200

    Empezando funcionalidad login

--- app/src/main/java/com/example/jimenaproject/MainActivity.kt
+++ app/src/main/java/com/example/jimenaproject/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.jimenaproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(1000)
         setTheme(R.style.Theme_JimenaProject)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 
     }
