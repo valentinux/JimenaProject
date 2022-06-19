7f1cf7bb76888a38db0adb6595825ee2325873e4
Valentinux <valentinux81@gmail.com>
2022-06-19 07:23:11 +0200

    Implementado viewbinding

--- app/src/main/java/com/example/jimenaproject/MainActivity.kt
+++ app/src/main/java/com/example/jimenaproject/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.jimenaproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jimenaproject.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(1000)
         setTheme(R.style.Theme_JimenaProject)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
 
