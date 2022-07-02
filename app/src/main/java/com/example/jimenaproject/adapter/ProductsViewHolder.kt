c06ed9cf256c186cc2db184716dddee87a617e69
Valentinux <valentinux81@gmail.com>
2022-07-02 07:39:03 +0200

    Cambio el diseño del Item_products

--- app/src/main/java/com/example/jimenaproject/adapter/ProductsViewHolder.kt
+++ app/src/main/java/com/example/jimenaproject/adapter/ProductsViewHolder.kt
@@ -12,8 +12,11 @@ class ProductsViewHolder(view:View): RecyclerView.ViewHolder(view) {
     val productDescription = view.findViewById<TextView>(R.id.tvProductDescription)
     val productphoto = view.findViewById<ImageView>(R.id.ivProduct)
 
+
     fun render(productsModel: ProductsModel){
         productName.text = productsModel.productname
         productDescription.text = productsModel.productdescription
+
+
     }
 }
