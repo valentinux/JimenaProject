19f236988f5f05207f35e5ba11e5e75f3003eb1a
Valentinux <valentinux81@gmail.com>
2022-07-02 06:55:03 +0200

    Cambio el diseño del Item_products

--- app/src/main/java/com/example/jimenaproject/ProductsProvider.kt
+++ app/src/main/java/com/example/jimenaproject/ProductsProvider.kt
@@ -8,7 +8,44 @@ class ProductsProvider {
                 "Producto de prueba",
                 "Este es un producto de prueba",
                 "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search"
-            )
+            ),
+            ProductsModel(
+                2,
+                "Producto de prueba2",
+                "Este es un producto de prueba2",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search2"
+            ),
+            ProductsModel(
+                3,
+                "Producto de prueba3",
+                "Este es un producto de prueba3",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search3"
+            ),
+            ProductsModel(
+                4,
+                "Producto de prueba4",
+                "Este es un producto de prueba4",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search4"
+            ),
+            ProductsModel(
+                1,
+                "Producto de prueba5",
+                "Este es un producto de prueba",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search"
+            ),
+            ProductsModel(
+                1,
+                "Producto de prueba6",
+                "Este es un producto de prueba",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search"
+            ),
+            ProductsModel(
+                1,
+                "Producto de prueba7",
+                "Este es un producto de prueba",
+                "https://www.freepik.es/vector-gratis/barras-piezas-chocolate_3139372.htm#query=colacao&position=5&from_view=search"
+            ),
+
         )
     }
 
