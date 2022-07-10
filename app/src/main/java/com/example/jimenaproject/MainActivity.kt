e65bb212e846d885c169b76aa6e6cefab6879cd4
Valentinux <valentinux81@gmail.com>
2022-07-10 07:27:59 +0200

    Implementado viewbinding y añadida funcionalidad (simple Toast) al botón addIcon

--- app/src/main/java/com/example/jimenaproject/MainActivity.kt
+++ app/src/main/java/com/example/jimenaproject/MainActivity.kt
@@ -6,22 +6,26 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jimenaproject.adapter.ProductsAdapter
+import com.example.jimenaproject.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(1000)
         setTheme(R.style.Theme_JimenaProject)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initRecyclerView()
     }
 
     private fun initRecyclerView(){
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerProducts)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ProductsAdapter(ProductsProvider.productsList)
+
+        binding.recyclerProducts.layoutManager = LinearLayoutManager(this)
+        binding.recyclerProducts.adapter = ProductsAdapter(ProductsProvider.productsList)
     }
 }
--- app/src/main/java/com/example/jimenaproject/adapter/ProductsViewHolder.kt
+++ app/src/main/java/com/example/jimenaproject/adapter/ProductsViewHolder.kt
@@ -1,24 +1,22 @@
 package com.example.jimenaproject.adapter
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.jimenaproject.ProductsModel
-
-import com.example.jimenaproject.R
+import com.example.jimenaproject.databinding.ItemProductsBinding
 
 class ProductsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    val productName = view.findViewById<TextView>(R.id.tvnameProduct)
-    val productDescription = view.findViewById<TextView>(R.id.tvProductDescription)
-    val productphoto = view.findViewById<ImageView>(R.id.ivProduct)
+    private val binding = ItemProductsBinding.bind(view)
 
 
     fun render(productsModel: ProductsModel) {
-        productName.text = productsModel.productname
-        productDescription.text = productsModel.productdescription
-        Glide.with(productphoto.context).load(productsModel.photo).into(productphoto)
+        binding.tvnameProduct.text = productsModel.productname
+        binding.tvProductDescription.text = productsModel.productdescription
+        Glide.with(binding.ivProduct.context).load(productsModel.photo).into(binding.ivProduct)
+        binding.IconAdd.setOnClickListener {
+            Toast.makeText(binding.IconAdd.context, "Añadido al desayuno", Toast.LENGTH_SHORT,).show() }
     }
 }
